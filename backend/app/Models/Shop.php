0f122dda009b79e94767d5ce0227b29b46d504f6
はまひつじ <r.m.c.f.789@gmail.com>
2020-10-20 18:35:41 +0900

    [Add]一覧ページ追加作成

--- backend/app/Models/Shop.php
+++ backend/app/Models/Shop.php
@@ -10,6 +10,6 @@ class Shop extends Model
     use HasFactory;
     public function category()
     {
-        return $this->belongsTo('App\Category');
+        return $this->belongsTo('App\Models\Category');
     }
 }
