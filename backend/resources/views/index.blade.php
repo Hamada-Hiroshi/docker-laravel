6903f450a453808e4982690b4e6ee6d23e7bca93
はまひつじ <r.m.c.f.789@gmail.com>
2020-10-20 19:04:47 +0900

    [Add]共通テンプレート作成、Bootstrap導入

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset='utf-8'>
-    <title>Lunchmap</title>
-    <style>body {padding: 10px;}</style>
-  </head>
-  <body>
-    <h1>お店一覧<h1>
-    @foreach($shops as $shop)
-      <p>
-        {{ $shop->category->name }},
-        {{ $shop->name }},
-        {{ $shop->address }}
-      </p>
-    @endforeach
-  </body>
-</html>
+@extends('layout')
+
+@section('content')
+  <h1>お店一覧</h1>
+  @foreach($shops as $shop)
+    <p>
+      {{ $shop->category->name }},
+      {{ $shop->name }},
+      {{ $shop->address }}
+    </p>
+  @endforeach
+@endsection
