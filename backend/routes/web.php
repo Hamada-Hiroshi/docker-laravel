73ccd2627e6fe420b75f6d77a7f831ab3b08c16f
はまひつじ <r.m.c.f.789@gmail.com>
2020-10-26 18:07:03 +0900

    [Add]詳細ページ追加作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ShopController;
 Route::get('/shops', [ShopController::class, 'index'])->name('shop.list');
+Route::get('/shop/{id}', [ShopController::class, 'show'])->name('shop.detail');
 
 Route::get('/', function () {
     return redirect('/shops');
