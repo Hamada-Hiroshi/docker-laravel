c144e43a84fb10db0a7955af719e35a34bb3c956
はまひつじ <r.m.c.f.789@gmail.com>
2020-10-27 16:50:14 +0900

    [Add]新規投稿ページ追加作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ShopController;
 Route::get('/shops', [ShopController::class, 'index'])->name('shop.list');
+Route::get('/shop/new', [ShopController::class, 'create'])->name('shop.new');
+Route::post('/shop', [ShopController::class, 'store'])->name('shop.store');
 Route::get('/shop/{id}', [ShopController::class, 'show'])->name('shop.detail');
 
 Route::get('/', function () {
