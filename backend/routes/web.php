b3a9d6b8a08d3b0fcc509a0ab18ff36a6efd70fa
はまひつじ <r.m.c.f.789@gmail.com>
2020-10-28 15:26:30 +0900

    [Add]削除機能追加

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,6 +9,7 @@ Route::post('/shop', [ShopController::class, 'store'])->name('shop.store');
 Route::get('/shop/edit/{id}', [ShopController::class, 'edit'])->name('shop.edit');
 Route::post('/shop/update/{id}', [ShopController::class, 'update'])->name('shop.update');
 Route::get('/shop/{id}', [ShopController::class, 'show'])->name('shop.detail');
+Route::delete('/shop/{id}', [ShopController::class, 'destroy'])->name('shop.destroy');
 
 Route::get('/', function () {
     return redirect('/shops');
